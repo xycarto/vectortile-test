c7fcb726480a3fe6a038a0d83f5d4f8d1c29d942
Ireese <ireese@linz.govt.nz>
2020-10-15 16:16:32 +1300

    add vector polys, update js

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-
-  
 var settings = {
     maxZoom: 19, 
     attribution: '<a href="http://www.linz.govt.nz">Sourced from LINZ. CC-BY 4.0</a>', //Simple attribution for linz
   };
 
-  var styles = {
-    paPointsWebmer: {	// Apply these options to the "water" layer...
-      //radius: 3,
-      fillColor: "#ff7800",
-      color: "#000",
-      weight: 1,
-      opacity: 1,
-      fillOpacity: 1.0
-    }
-  };
+var styles = {
+  interactive: true,
+  vectorTileLayerStyles: {
+    LiDAR_available_now: {
+      weight: 0,
+      fillColor: "#e4a5e4",
+      fillOpacity: 0.75,
+      fill: true
+  }}
+};
 
-var url = "https://xycarto.github.io/vectortile-test/tippe/{z}/{x}/{y}.pbf";
+//var url = "http://localhost:8000/tippe/{z}/{x}/{y}.pbf";
 
-var vector = L.vectorGrid.protobuf(url, {
-  vectorTileLayerStyles: styles,
-  subdomains: '0123'
-});
+var url = "https://xycarto.github.io/vectortile-test/available_now/{z}/{x}/{y}.pbf";
+
+var vector = L.vectorGrid.protobuf(url, styles);
 
 var basemap = new L.TileLayer('https://tiles.maps.linz.io/nz_colour_basemap/GLOBAL_MERCATOR/{z}/{x}/{y}.png', settings)
-  
+
 var map = new L.Map('map',
  {center: [-39.9, 175.2], 
  zoom: 6,
