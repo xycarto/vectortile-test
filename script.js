0363fba629b06d545acc9bbd0d3ee6e45d92495d
Ireese <ireese@linz.govt.nz>
2020-10-27 09:27:36 +1300

    add highlight

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-var settings = {
+var settingsBasemap = {
     maxZoom: 19, 
     attribution: '<a href="http://www.linz.govt.nz">Sourced from LINZ. CC-BY 4.0</a>', //Simple attribution for linz
-  };
+};
 
  
-  var styles = {
+//Vector tiles
+
+//layer url
+var urlVector = "https://xycarto.github.io/vectortile-test/available_now/{z}/{x}/{y}.pbf";
+
+var stylesStart = {
     interactive: true,
     getFeatureID: function(f) {
       return f.layer.properties;
@@ -23,42 +28,42 @@ var settings = {
             fill: true
           }
     }}
-  };
-
-  /*
-  var highlight;
-  var clearHighlight = function() {
-    if (highlight) {
-      vector.resetFeatureStyle(highlight);
-    }
-    highlight = null;
-  };
-  */
-
-  //var url = "http://localhost:8000/available_now/{z}/{x}/{y}.pbf";
-
-//L.tileLayer('https://xycarto.github.io/vectortile-test/available_now/{z}/{x}/{y}.pbf')
-  
-var url = "https://xycarto.github.io/vectortile-test/available_now/{z}/{x}/{y}.pbf";
+};
 
 
-var vector = L.vectorGrid.protobuf(url, styles)
-  .on('click', function(e) {
+var vector = L.vectorGrid.protobuf(urlVector, stylesStart)
+.on('click', function(e) {
     L.popup()
       .setContent('Name: ' + e.layer.properties.name + ' <br></br>' + 'DEM: ' + e.layer.properties.DataDEM)
       .setLatLng(e.latlng)
       .openOn(map);
+})
+.on("mouseover", function(e) {
+  e.layer.setStyle({
+    fillColor: "yellow"
+  })
+})
+.on("mouseout", function(e) {
+  e.layer.setStyle({
+    //weight: weight,
+    opacity: 1,
+    color: "#d677d6",
+    fillColor: "#e4a5e4",
+    fillOpacity: 0.75,
+    fill: true
   });
+});
 
-var basemap = new L.TileLayer('https://tiles.maps.linz.io/nz_colour_basemap/GLOBAL_MERCATOR/{z}/{x}/{y}.png', settings)
+//Base map
+var basemap = new L.TileLayer('https://tiles.maps.linz.io/nz_colour_basemap/GLOBAL_MERCATOR/{z}/{x}/{y}.png', settingsBasemap)
 
-//map.on('click', clearHighlight);
 
+//build all maps
 var map = new L.Map('map',
  {center: [-39.9, 175.2], 
  zoom: 6,
  layers: [basemap, vector]
- }); 
+}); 
 
 map.addLayer(map);
 
